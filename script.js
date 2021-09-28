a55e2f2ff2fe056c5dfd8c557743505ad372163c
sharkout <shahrukh_04@live.com>
2021-09-28 14:13:36 +0000

    App is ready.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit " + "<mark>" + principal + "</mark>"+ " at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + ". You will receive an amount of " + "<mark>" + interest + "</mark>" + ", in the year " + "<mark>" + year + "</mark>";
+
 }
-        
